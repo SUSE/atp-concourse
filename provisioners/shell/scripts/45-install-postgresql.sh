be758670fc38abd51a7f6d53c13063c2318148be
Chris Olstrom <chris@olstrom.com>
2017-09-15 15:56:05 -0700

    Start postgresql.service once for initial setup

--- provisioners/shell/scripts/45-install-postgresql.sh
+++ provisioners/shell/scripts/45-install-postgresql.sh
@@ -4,4 +4,7 @@
 zypper refresh
 zypper --non-interactive install postgresql postgresql-server
 
+systemctl start postgresql.service
+systemctl stop postgresql.service
+
 augtool set /files/var/lib/pgsql/data/pg_hba.conf/1/method trust
